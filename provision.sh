e503ea2f1752fa67e26b12a31a67155def5399d4
masiuchi <masahiro.iuchi@gmail.com>
2016-04-23 12:11:44 +0900

    Install Composer and add permission to read PHP file in /home/vagrant.

--- provision.sh
+++ provision.sh
@@ -65,3 +65,12 @@ if [[ ! -f /var/www/html/index.php ]]; then
   echo "<?php phpinfo(); ?>" >> /var/www/html/index.php
 fi
 
+# https://getcomposer.org/download/
+php -r "readfile('https://getcomposer.org/installer');" > composer-setup.php
+php -r "if (hash_file('SHA384', 'composer-setup.php') === '7228c001f88bee97506740ef0888240bd8a760b046ee16db8f4095c0d8d525f2367663f22a46b48d072c816e7fe19959') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+mv composer.phar /usr/local/bin/composer
+
+chmod 701 /home/vagrant
+
